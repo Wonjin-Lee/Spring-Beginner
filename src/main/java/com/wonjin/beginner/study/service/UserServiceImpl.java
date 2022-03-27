e233b20ae4a44a011e652343258adaea948e6f9d
Wonjin-Lee <claude17@naver.com>
2022-03-27 22:42:01 +0900

    Lookup 어노테이션을 활용하여 의존 관계 정의

--- src/main/java/com/wonjin/beginner/study/service/UserServiceImpl.java
+++ src/main/java/com/wonjin/beginner/study/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.wonjin.beginner.study.exception.UserAlreadyRegisteredException;
 import com.wonjin.beginner.study.repository.UserRepository;
 import com.wonjin.beginner.study.support.PasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -24,9 +25,10 @@ public class UserServiceImpl implements UserService {
         String encodedPassword = passwordEncoder.encode(rawPassword);
     }
 
-    // ApplicationContext를 통해 DI 컨테이너에 등록된 빈을 직접 찾아서 가져온다.
+    // 룩업 메서드 인젝션을 활용하여 의존 관계 정의
     // 이때 꺼내오는 빈은 원래 의도한 스코프(prototype)대로 설정되어 나온다.
+    @Lookup
     PasswordEncoder passwordEncoder() {
-        return this.context.getBean(PasswordEncoder.class);
+        return null;
     }
 }
